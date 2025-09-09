8573fa7f8dc580f84a0a50b6762446c593ded014
13R1ch13 <richaa0aard@gmail.com>
2025-09-09 16:15:59 +0300

    Implement order handling and saving?

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 from db.database import init_db
 from handlers import ua
+from handlers import order
 from config import BOT_TOKEN
 
 async def main():
@@ -13,6 +14,7 @@ async def main():
     init_db()
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(ua.router)
+    dp.include_router(order.router)
     bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     await dp.start_polling(bot)
 
